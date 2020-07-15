7d1333800e9e27d42f98785045277fe65d3ac243
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-16 00:22:34 +0200

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>{{title}}</h1>
     <v-btn color="primary" @click="test">test</v-btn>
+    <p>{{this.response}}</p>
   </div>
 </template>
 
@@ -11,13 +12,15 @@ import TestService from '@/services/TestService'
 export default{
   data(){
     return {
-      title: process.env.VUE_APP_TITLE
+      title: process.env.VUE_APP_TITLE,
+      response: "ciao"
     }
   },
   methods: {
     async test(){
       try {
       const response = await TestService.test()
+      this.response = response.data.msg
       } catch (error) {
         this.error = error.response.data.error;
       }
