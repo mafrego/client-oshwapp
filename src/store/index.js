f45234332eb2e752a7df8374971086464500ab28
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-07-25 22:25:30 +0200

    added users module

--- src/store/index.js
+++ src/store/index.js
@@ -2,43 +2,24 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import todos from './modules/todos'
+import users from './modules/users'
 
 Vue.use(Vuex)
 
+// const userState = createPersistedState({
+//   paths: ['token', 'user', 'isUserLoggedIn']
+// })
+
 export default new Vuex.Store({
   modules: {
-    todos
+    todos,
+    users
   },
   strict: true,
+
+  // plugins: [userState]
+
   plugins: [
     createPersistedState()
   ],
-  // TODO create module for the following stuff()
-  state: {
-    token: null,
-    user: null,
-    isUserLoggedIn: false
-  },
-  mutations: {
-    setToken (state, token) {
-      state.token = token
-      // state.isUserLoggedIn = !!(token)
-      if(token){
-        state.isUserLoggedIn = true
-      }else{
-        state.isUserLoggedIn = false
-      }
-    },
-    setUser (state, user) {
-      state.user = user
-    }
-  },
-  actions: {
-    setToken ({commit}, token) {
-      commit('setToken', token)
-    },
-    setUser ({commit}, user) {
-      commit('setUser', user)
-    }
-  }
 })
