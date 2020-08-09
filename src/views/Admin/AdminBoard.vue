f311872738da527abb86e590be198a13e7253cd0
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-09 15:06:46 +0200

    added components

--- src/views/Admin/AdminBoard.vue
+++ src/views/Admin/AdminBoard.vue
@@ -1,13 +1,22 @@
 <template>
 <div>
-    users
-    projects
+    <admin-board-users>
+    </admin-board-users>
+    <admin-board-projects>
+    </admin-board-projects>
 </div>
 </template>
 
 <script>
+import AdminBoardUsers from "./AdminBoardUsers";
+import AdminBoardProjects from "./AdminBoardProjects";
+
 export default {
-    name: "AdminBoard"
+    name: "AdminBoard",
+    components: {
+        AdminBoardUsers,
+        AdminBoardProjects
+    }
     
 }
 </script>
