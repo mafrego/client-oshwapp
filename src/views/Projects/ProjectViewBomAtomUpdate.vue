22d4e469fd9d831b1031a2929e19185c3bf73ba6
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-13 11:02:49 +0200

    created

---
+++ src/views/Projects/ProjectViewBomAtomUpdate.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    {{atom.name}} {{atom.uuid}}
+</div>
+</template>
+
+<script>
+export default {
+    name: "ProjectViewBomAtomDetails",
+    props: {
+        atom: {
+            type: Object,
+            required: true,
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
