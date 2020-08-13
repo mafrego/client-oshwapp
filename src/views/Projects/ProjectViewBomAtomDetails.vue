e7ac104ab66dd382c7e7935f761d99aacad665da
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-13 11:38:02 +0200

    added property list

--- src/views/Projects/ProjectViewBomAtomDetails.vue
+++ src/views/Projects/ProjectViewBomAtomDetails.vue
@@ -1,22 +1,43 @@
 <template>
-<div>
-    {{atom.name}} {{atom.uuid}}
-</div>
+  <div>
+    <ul>
+      <li v-for="(value, name) in atom" :key="name">
+        {{name}}: {{value}}
+        <!-- <ul>
+          <li>name: {{atom.name}}</li>
+          <li>uuid: {{atom.uuid}}</li>
+          <li>code: {{atom.code}}</li>
+          <li>description: {{atom.description}}</li>
+          <li>link: {{atom.link}}</li>
+          <li>vendor URL:{{atom.vendorUrl}}</li>
+          <li>minimum order quantity: {{atom.moq}}</li>
+          <li>lead time: {{atom.leadTime}}</li>
+          <li>notes: {{atom.notes}}</li>
+          <li>quantity: {{atom.quantity}}</li>
+          <li>quantity left to assemble: {{atom.quantity_to_assemble}}</li>
+          <li>material: {{atom.material}}</li>
+          <li>weight: {{atom.weight}}</li>
+          <li>weight unit: {{atom.weightUnit}}</li>
+          <li>cost per unit: {{atom.costUnit}}</li>
+          <li>currency: {{atom.currency}}</li>
+          <li>image URL: {{atom.imageUrl}}</li>
+        </ul> -->
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ProjectViewBomAtomDetails",
-    props: {
-        atom: {
-            type: Object,
-            required: true,
-        }
-    }
-    
-}
+  name: "ProjectViewBomAtomDetails",
+  props: {
+    atom: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
