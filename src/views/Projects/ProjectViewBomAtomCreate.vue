3390ec03f50ddb84d91be2576bf4d28ff60f5f60
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-18 11:29:31 +0200

    fixed problem with empty fields

--- src/views/Projects/ProjectViewBomAtomCreate.vue
+++ src/views/Projects/ProjectViewBomAtomCreate.vue
@@ -2,7 +2,7 @@
   <div>
     <panel title="Create atom">
         <!-- TODO add input fields with relative rules -->
-      <v-text-field label="name" :rules="[rules.required]" v-model="atom.name" id="id"></v-text-field>
+      <v-text-field label="name" :rules="[rules.required, rules.isAlphanumeric]" v-model="atom.name" id="id"></v-text-field>
       <v-text-field
         label="description"
         :rules="[rules.required]"
@@ -17,16 +17,14 @@
         min="1"
         id="id"
       ></v-text-field>
-      <v-text-field label="link" :rules="[rules.required, rules.http]" v-model="atom.link" id="id"></v-text-field>
+      <v-text-field label="link" :rules="[rules.http]" v-model="atom.link" id="id"></v-text-field>
       <v-text-field
         label="weight"
-        :rules="[rules.required, rules.float]"
+        :rules="[rules.float]"
         v-model="atom.weight"
         id="id"
       ></v-text-field>
       <v-text-field label="material" :rules="[rules.required]" v-model="atom.material" id="id"></v-text-field>
-      <!-- TODO multiple choice weight units -->
-      <v-text-field label="weight unit" :rules="[rules.required]" v-model="atom.weightUnit" id="id"></v-text-field>
     </panel>
     <div class="green--text" v-if="message">{{message}}</div>
     <div class="red--text" v-if="error">{{error}}</div>
@@ -53,17 +51,25 @@ export default {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "Invalid e-mail.";
         },
+        isAlphanumeric: (value) => {
+          const pattern = /^[-0-9a-zA-Z_]+$/;
+          if(value) return pattern.test(value) || "Only alphanumeric, hyphens or underscores chars!";
+          else return true
+        },
         http: (value) => {
           const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-          return pattern.test(value) || "Invalid http link";
+          if(value) return pattern.test(value) || "Invalid http link";
+          else return true
         },
         float: (value) => {
           const pattern = /^[+-]?\d+(\.\d+)?$/;
-          return pattern.test(value) || "Invalid float";
+          if(value) return pattern.test(value) || "Invalid float";
+          else return true
         },
         positiveInt: (value) => {
           const pattern = /^[1-9]+[0-9]*$/;
-          return pattern.test(value) || "only positive integers > 0";
+          if(value) return pattern.test(value) || "only positive integers > 0";
+          else return true
         }
       }
     }
