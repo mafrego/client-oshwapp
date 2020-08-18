3a2920e66ff256c932b81e4df302db1c5d771875
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-18 11:28:43 +0200

    still to finish

--- src/views/Projects/ProjectViewBomAtomUpdate.vue
+++ src/views/Projects/ProjectViewBomAtomUpdate.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <v-btn color="blue" @click="loadData(atom)" title="actual values">
+    <!-- <v-btn color="blue" @click="loadData(atom)" title="actual values">
       <v-icon>refresh</v-icon>
-    </v-btn>
+    </v-btn> -->
     <v-layout column>
       <v-flex xs1>
-        <v-text-field v-model="atomToUpdate.description" label="description"></v-text-field>
-        <!-- TODO if quantity can be updated then update quantity_to_assemble as well -->
-        <!-- <v-text-field v-model="atomToUpdate.quantity" label="quantity" type="number" min="1"></v-text-field> -->
-        <v-text-field v-model="atomToUpdate.cost" label="cost" type="number" min="0" step="any"></v-text-field>
-        <v-text-field v-model="atomToUpdate.currency" label="currency"></v-text-field>
-        <v-text-field v-model="atomToUpdate.link" label="link"></v-text-field>
-        <v-text-field v-model="atomToUpdate.vendorUrl" label="vendor"></v-text-field>
-        <v-text-field v-model="atomToUpdate.moq" label="mimimum quantity order"></v-text-field>
-        <v-text-field v-model="atomToUpdate.leadTime" label="lead time"></v-text-field>
-        <v-text-field v-model="atomToUpdate.material" label="material"></v-text-field>
-        <v-text-field v-model="atomToUpdate.weight" label="weight"></v-text-field>
-        <v-text-field v-model="atomToUpdate.notes" label="notes"></v-text-field>
+        <!-- name, code, quantity, imageUrl cannot be updated -->
+        <v-text-field v-model="description" :rules="[rules.isDescription]" label="description"></v-text-field>
+        <v-text-field v-model="cost" :rules="[rules.isPositiveFloat]" label="cost" ></v-text-field>
+        <v-text-field v-model="currency" :rules="[rules.isCurrency]" label="currency"></v-text-field>
+        <v-text-field v-model="link" :rules="[rules.isURL]" label="link"></v-text-field>
+        <v-text-field v-model="vendorUrl" :rules="[rules.isURL]" label="vendor"></v-text-field>
+        <v-text-field v-model="moq" :rules="[rules.positiveInt]" label="mimimum quantity order"></v-text-field>
+        <v-text-field v-model="leadTime" :rules="[rules.isDuration]" label="lead time"></v-text-field>
+        <v-text-field v-model="material" :rules="[rules.isAlphanumeric]" label="material"></v-text-field>
+        <v-text-field v-model="weight" :rules="[rules.positiveFloat]" label="weight"></v-text-field>
+        <v-text-field v-model="notes" :rules="[rules.isDescription]" label="notes"></v-text-field>
 
         <v-btn class="yellow" @click="update()">
           <v-icon>save</v-icon>
@@ -37,32 +36,124 @@ export default {
   name: "ProjectViewBomAtomUpdate",
   data() {
     return {
-      atomToUpdate: {},
       message: "",
       error: "",
+      rules: {
+        required: (value) => !!value || "Required.",
+        isDescription: (value) => {
+          const pattern = /^[-a-zA-Z0-9 _.]*$/;
+          if(value) return pattern.test(value) || "Only alphanumeric, dots, hyphens, underscore chars";
+          else return true
+        },
+        isURL: (value) => {
+          const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+          if(value) return pattern.test(value) || "invalid URL";
+          else return true
+        },
+        isPositiveFloat: (value) => {
+          const pattern = /^(?:[1-9]\d*|0)?(?:\.\d+)?$/;
+          if(value) return pattern.test(value) || "only positive float";
+          else return true
+        },
+        isPositiveInt: (value) => {
+          const pattern = /^[1-9]+[0-9]*$/;
+          if(value) return pattern.test(value) || "only positive integers > 0";
+          else return true
+        }
+      }
     };
   },
-  props: {
-    atom: {
-      type: Object,
-      required: true,
-    },
+  computed: {
+      description: {
+        get() {
+          return this.$store.state.projects.atom.description
+        },
+        set (value) {
+          this.$store.commit('updateAtomDescription', value)
+        }
+      },
+      cost: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      currency: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      link: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      vendorUrl: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      moq: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      leadTime: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      material: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      weight: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
+      notes: {
+        get() {
+          return this.$store.state.projects.atom.cost
+        },
+        set (value) {
+          this.$store.commit('updateAtomCost', value)
+        }
+      },
   },
   methods: {
     ...mapGetters(["getAtom"]),
     ...mapActions(["reviseAtom"]),
-    loadData(data) {
-      // ATTENTION!!!! shallow copy so that no vuex error
-      let tmp = Object.assign({}, data);
-      this.atomToUpdate = tmp;
-    },
     async update() {
       // console.log(this.atomToUpdate.description)
       try {
         this.message = "";
         this.error = "";
-        console.log("calling update");
-        const response = await this.reviseAtom(this.atomToUpdate);
+        const response = await this.reviseAtom();
         if (response.status === 200) {
           this.message = "atom updated";
         } else {
