bd3f2cf972db72fd386f1b737ce943a774150f0b
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-27 17:01:31 +0200

    removed comments

--- src/views/Projects/ProjectViewBomAtomDetails.vue
+++ src/views/Projects/ProjectViewBomAtomDetails.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <!-- <ul>
-      <li v-for="(value, name) in atom" :key="name">
-        {{name}}: {{value}} -->
         <ul>
-          <!-- <li>name: {{atom.name}}</li> -->
-          <!-- <li>uuid: {{atom.uuid}}</li> -->
           <li >item #{{atom.itemNumber}}</li>
           <li v-if="atom.description" >description: {{atom.description}}</li>
           <li>minimum order quantity: {{atom.moq}}</li>
@@ -17,9 +12,8 @@
           <li v-if="atom.leadTime">lead time: {{atom.leadTime}}</li>
           <li v-if="atom.link" ><a :href="atom.link" target="_blank" >official page</a></li>
           <li v-if="atom.notes">notes: {{atom.notes}}</li>
+          <!-- <li><a :href="atom.imageUrl" download target="_blank">download image</a></li> -->
         </ul>
-      <!-- </li>
-    </ul> -->
   </div>
 </template>
 
