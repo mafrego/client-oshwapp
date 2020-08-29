cc5263052cd541ea1e238dd5b77071c4876a979a
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-29 09:27:38 +0200

    fixed form logic

--- src/views/Projects/ProjectViewBomAtomCreate.vue
+++ src/views/Projects/ProjectViewBomAtomCreate.vue
@@ -1,35 +1,56 @@
 <template>
   <div>
     <panel title="Create atom">
-        <!-- TODO add input fields with relative rules -->
-      <v-text-field label="name" :rules="[rules.required, rules.isAlphanumeric]" v-model="atom.name" ></v-text-field>
-      <v-text-field label="description" :rules="[rules.required, rules.isDescription]" v-model="atom.description"></v-text-field>
-      <v-text-field label="quantity" :rules="[rules.required, rules.isPositiveInt]" v-model="atom.quantity"></v-text-field>
-      <v-text-field label="cost" :rules="[rules.required, rules.isPositiveFloat]" v-model="atom.cost" ></v-text-field>
-      <v-text-field label="currency" :rules="[rules.required, rules.isCurrency]" v-model="atom.currency" ></v-text-field>
-      <v-text-field label="code" :rules="[rules.required, rules.isAlphanumeric]" v-model="atom.code" ></v-text-field>
-      <v-text-field label="link" :rules="[rules.http]" v-model="atom.link" ></v-text-field>
-      <v-text-field label="vendor URL" :rules="[rules.http]" v-model="atom.vendorUrl" ></v-text-field>
-      <v-text-field label="minimum order quantity" :rules="[rules.isPositiveInt]" v-model="atom.moq" ></v-text-field>
-      <v-text-field label="lead time" :rules="[rules.isDuration]" v-model="atom.leadTime" ></v-text-field>
-      <v-text-field label="material" :rules="[rules.isAlphanumeric]" v-model="atom.material" ></v-text-field>
-      <v-text-field label="weight" :rules="[rules.isPositiveFloat]" v-model="atom.weight"></v-text-field>
+      <v-form ref="form">
+        <!-- check if atom name is already taken with getBOM -->
+      <v-text-field
+        label="name"
+        :rules="[rules.required, rules.isAlphanumeric]"
+        v-model="atom.name"
+      ></v-text-field>
+      <v-text-field
+        label="description"
+        :rules="[rules.required, rules.isDescription]"
+        v-model="atom.description"
+      ></v-text-field>
+      <v-text-field
+        label="minimum order quantity"
+        :rules="[rules.required, rules.isPositiveInt]"
+        v-model="atom.moq"
+      ></v-text-field>
+      <v-text-field
+        label="quantity"
+        :rules="[rules.required, rules.isPositiveInt]"
+        v-model="atom.quantity"
+      ></v-text-field>
+      <v-text-field
+        label="unit cost"
+        :rules="[rules.required, rules.isPositiveFloat]"
+        v-model="atom.unitCost"
+      ></v-text-field>
+      <v-text-field
+        label="currency"
+        :rules="[rules.required, rules.isCurrency]"
+        v-model="atom.currency"
+      ></v-text-field>
+      <v-text-field label="GTIN" :rules="[rules.isAlphanumeric]" v-model="atom.GTIN"></v-text-field>
+      <v-text-field label="SKU" :rules="[rules.isAlphanumeric]" v-model="atom.SKU"></v-text-field>
+      <v-text-field label="vendor URL" :rules="[rules.isHTTP]" v-model="atom.vendorUrl"></v-text-field>
+      <v-text-field label="lead time" :rules="[rules.isDuration]" v-model="atom.leadTime"></v-text-field>
+      <v-text-field label="link" :rules="[rules.isHTTP]" v-model="atom.link"></v-text-field>
       <v-text-field label="notes" :rules="[rules.isDescription]" v-model="atom.notes" id="id"></v-text-field>
+      </v-form>
     </panel>
     <div class="green--text" v-if="message">{{message}}</div>
     <div class="red--text" v-if="error">{{error}}</div>
-    <v-btn 
-      class="green" 
-      @click="create"
-      title="save atom"
-     >
+    <v-btn class="green" @click="create" title="save atom">
       <v-icon>save</v-icon>
     </v-btn>
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
   name: "ProjectViewBomAtomCreate",
@@ -47,58 +68,86 @@ export default {
         },
         isDescription: (value) => {
           const pattern = /^[^,;]+$/;
-          if(value) return pattern.test(value) || "any char but commas and semicolons"
-          else return true
+          if (value)
+            return pattern.test(value) || "any char but commas and semicolons";
+          else return true;
         },
         isCurrency: (value) => {
           const pattern = /[A-Z]{3}/;
-          if(value) return pattern.test(value) || "only currency ISO 4217 e.g. EUR, USD";
-          else return true
+          if (value)
+            return (
+              pattern.test(value) || "only currency ISO 4217 e.g. EUR, USD"
+            );
+          else return true;
         },
         isAlphanumeric: (value) => {
           const pattern = /^[-0-9a-zA-Z_]+$/;
-          if(value) return pattern.test(value) || "only alphanumeric hyphens underscores";
-          else return true
+          if (value)
+            return (
+              pattern.test(value) || "only alphanumeric hyphens underscores"
+            );
+          else return true;
         },
-        http: (value) => {
+        isHTTP: (value) => {
           const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-          if(value) return pattern.test(value) || "Invalid http link";
-          else return true
+          if (value) return pattern.test(value) || "Invalid HTTP link";
+          else return true;
         },
         isPositiveFloat: (value) => {
           const pattern = /^[+-]?\d+(\.\d+)?$/;
-          if(value) return pattern.test(value) || "Invalid float";
-          else return true
+          if (value) return pattern.test(value) || "Invalid float";
+          else return true;
         },
         isPositiveInt: (value) => {
           const pattern = /^[1-9]+[0-9]*$/;
-          if(value) return pattern.test(value) || "only positive integer > 0";
-          else return true
+          if (value) return pattern.test(value) || "only positive integer > 0";
+          else return true;
         },
         isDuration: (value) => {
           const pattern = /^P(?!$)(\d+(?:\.\d+)?Y)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?W)?(\d+(?:\.\d+)?D)?(T(?=\d)(\d+(?:\.\d+)?H)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?S)?)?$/;
-          if(value) return pattern.test(value) || "only duration ISO 8601";
-          else return true
-        }
-      }
-    }
+          if (value) return pattern.test(value) || "only duration ISO 8601";
+          else return true;
+        },
+      },
+    };
   },
-    methods: {
-        ...mapActions(["createAtom"]),
-        async create(){
-            this.message = ""
-            this.error = ""
-            const response = await this.createAtom(this.atom)
-            // console.log(response)
-            if(response.status === 201){
-                this.message = "added new atom to BOM"
-            } else {
-                this.error = "problems in creating new atom"
-            }
-        }
-    }
-};
+  methods: {
+    ...mapActions(["createAtom"]),
+    async create() {
+      this.message = "";
+      this.error = "";
+      if (
+        this.atom.name &&
+        this.atom.description &&
+        this.atom.moq &&
+        this.atom.quantity &&
+        this.atom.unitCost &&
+        this.atom.currency
+      ) {
+        this.error = null;
+      } else {
+        this.error = "Please fill in all the required fields.";
+        return;
+      }
+      const response = await this.createAtom(this.atom);
+      // console.log(response)
+      if (response.status === 201) {
+        this.message = "added new atom to BOM";
 
+          this.atom.name = null
+          this.atom.description = null
+          this.atom.moq = null
+          this.atom.quantity = null
+          this.atom.unitCost = null
+          this.atom.currency = null
+          this.$refs.form.resetValidation();
+
+      } else {
+        this.error = "problems in creating new atom";
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
--- src/views/Projects/ProjectsPanelCreate.vue
+++ src/views/Projects/ProjectsPanelCreate.vue
@@ -1,168 +1,178 @@
 <template>
   <div>
     <panel title="Project Metadata">
-      <v-form ref="form" > 
-      <v-text-field
-        label="project name"
-        :rules="[rules.required, rules.singleName, rules.isAlphanumeric]"
-        v-model="project.name"
-        id="id"
-      ></v-text-field>
-      <v-text-field
-        label="description"
-        :rules="[rules.required, rules.isDescription]"
-        v-model="project.description"
-        id="id"
-      ></v-text-field>
-      <v-text-field
-        label="version"
-        :rules="[rules.required, rules.isSemanticVersion]"
-        v-model="project.version"
-        id="id"
-      ></v-text-field>
-      <!-- add drop down list for license -->
-      <v-text-field
-        label="license"
-        :rules="[rules.required, rules.isAlphanumeric]"
-        v-model="project.license"
-        id="id"
-      ></v-text-field>
-      <v-text-field
-        label="country"
-        :rules="[rules.isISO31661]"
-        v-model="project.country"
-        id="id"
-      ></v-text-field>
-      <v-text-field
-        label="region"
-        :rules="[rules.isISO31662]"
-        v-model="project.region"
-        id="id"
-      ></v-text-field>
-      <v-text-field
-        label="project link"
-        :rules="[rules.isHTTP]"
-        v-model="project.projectUrl"
-        id="id"
-      ></v-text-field>
-      <!-- <v-text-field
-        label="bop link"
-        :rules="[rules.isHTTP]"
-        v-model="project.bopUrl"
-        id="id"
-      ></v-text-field> -->
+      <v-form ref="form">
+        <v-text-field
+          label="project name"
+          :rules="[rules.required, rules.singleName, rules.isAlphanumeric]"
+          v-model="project.name"
+          id="id"
+        ></v-text-field>
+        <v-text-field
+          label="description"
+          :rules="[rules.required, rules.isDescription]"
+          v-model="project.description"
+          id="id"
+        ></v-text-field>
+        <v-text-field
+          label="version"
+          :rules="[rules.required, rules.isSemanticVersion]"
+          v-model="project.version"
+          id="id"
+        ></v-text-field>
+        <!-- add drop down list for license -->
+        <v-text-field
+          label="license"
+          :rules="[rules.required, rules.isAlphanumeric]"
+          v-model="project.license"
+          id="id"
+        ></v-text-field>
+        <v-text-field label="country" :rules="[rules.isISO31661]" v-model="project.country" id="id"></v-text-field>
+        <v-text-field label="region" :rules="[rules.isISO31662]" v-model="project.region" id="id"></v-text-field>
+        <v-text-field
+          label="project link"
+          :rules="[rules.isHTTP]"
+          v-model="project.projectUrl"
+          id="id"
+        ></v-text-field>
       </v-form>
     </panel>
     <div class="danger-alert" v-if="error">{{error}}</div>
+    <div class="green--text" v-if="message">{{message}}</div>
     <v-btn class="green" @click="create" title="save">
       <v-icon>save</v-icon>
-      </v-btn>
-          <v-progress-circular
-            class="ml-10"
-            v-if="getLoading"
-            :indeterminate="getLoading"
-            color="light-blue"
-          ></v-progress-circular>
-          <br />
+    </v-btn>
+    <v-progress-circular
+      class="ml-10"
+      v-if="getLoading"
+      :indeterminate="getLoading"
+      color="light-blue"
+    ></v-progress-circular>
+    <br />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
-import semverRegex from 'semver-regex'
-import iso31661 from 'iso-3166'
-import iso31662 from 'iso-3166/2'
+import semverRegex from "semver-regex";
+import iso31661 from "iso-3166";
+import iso31662 from "iso-3166/2";
 
 export default {
   data() {
     return {
       project: {
-        name: "",
-        description: "",
-        version: "",
-        license: "",
-        country: "",
-        region: "",
-        projectUrl: "",
-        bopUrl: "",
+        name: null,
+        description: null,
+        version: null,
+        license: null,
+        country: null,
+        region: null,
+        projectUrl: null,
         userID: this.$store.state.user.uuid,
       },
-      error: null,
+      error: "",
+      message: "",
       rules: {
         required: (value) => !!value || "Required.",
         singleName: (value) =>
           !this.getProjectNames.includes(value) || "name already taken!",
         isAlphanumeric: (value) => {
           const pattern = /^[-0-9a-zA-Z_]+$/;
-          if(value) return pattern.test(value) || "only alphanumeric hyphens underscores";
-          else return true
+          if (value)
+            return (
+              pattern.test(value) || "only alphanumeric hyphens underscores"
+            );
+          else return true;
         },
         string: (value) => {
           const pattern = /^[0-9a-zA-Z_]+$/;
-          return pattern.test(value) || "only alphanumericals and underscores allowed";
+          return (
+            pattern.test(value) ||
+            "only alphanumericals and underscores allowed"
+          );
         },
         isDescription: (value) => {
           const pattern = /^[^,;]+$/;
-          if(value) return pattern.test(value) || "any char but commas and semicolons"
-          else return true
+          if (value)
+            return pattern.test(value) || "any char but commas and semicolons";
+          else return true;
         },
         isSemanticVersion: (value) => {
-          if(value) return semverRegex().test(value) || "e.g. 0.0.1"
-          else return true
+          if (value) return semverRegex().test(value) || "e.g. 0.0.1";
+          else return true;
         },
         isHTTP: (value) => {
           const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-          if(value) return pattern.test(value) || "Invalid http link";
-          else return true
+          if (value) return pattern.test(value) || "Invalid http link";
+          else return true;
         },
         isISO31661: (value) => {
-          let ret = false
-          iso31661.forEach(element => { 
-            if(element.alpha2 == value || element.alpha3 === value || element.numeric === value || element.name === value) 
-            ret = true
-            });
-          if(value) return ret || "ISO 3166-1 e.g. IT, ITA, 380 or Italy";
-          else return true
+          let ret = false;
+          iso31661.forEach((element) => {
+            if (
+              element.alpha2 == value ||
+              element.alpha3 === value ||
+              element.numeric === value ||
+              element.name === value
+            )
+              ret = true;
+          });
+          if (value) return ret || "ISO 3166-1 e.g. IT, ITA, 380 or Italy";
+          else return true;
         },
         isISO31662: (value) => {
-          let ret = false
-          iso31662.forEach(element => { 
-            if(element.code == value || element.name === value) 
-            ret = true
-            });
-          if(value) return ret || "ISO 3166-2 e.g. for county Wicklow in Ireland IE-WW or Wicklow";
-          else return true
+          let ret = false;
+          iso31662.forEach((element) => {
+            if (element.code == value || element.name === value) ret = true;
+          });
+          if (value)
+            return (
+              ret ||
+              "ISO 3166-2 e.g. for county Wicklow in Ireland IE-WW or Wicklow"
+            );
+          else return true;
         },
       },
     };
   },
   computed: {
     ...mapGetters([
-      "getProjectNames", 
-      "getProjects",
-      "getLoading"
-      ]),
+      "getProjectNames",
+      "getLoading",
+    ]),
   },
   methods: {
     ...mapActions(["createProject"]),
     async create() {
       this.error = null;
-      const areAllFieldsFilledIn = Object.keys(this.project).every(
-        (key) => !!this.project[key]
-      );
-      if (!areAllFieldsFilledIn) {
+      this.message = null;
+      if (
+        this.project.name &&
+        this.project.description &&
+        this.project.version &&
+        this.project.license
+      ) {
+        this.error = null;
+      } else {
         this.error = "Please fill in all the required fields.";
         return;
       }
       try {
         this.project.state = "created";
         this.project.dateTime = new Date();
-        const response = await this.createProject(this.project)
-        if(response == 201){
+        const response = await this.createProject(this.project);
+        if (response == 201) {
+          this.message = "project created";
 
-          this.project.name = null
-          this.project.description = null
-          this.$refs.form.resetValidation()
+          this.project.name = null;
+          this.project.description = null;
+          this.project.version = null;
+          this.project.license = null;
+          this.project.country = null;
+          this.project.region = null;
+          this.project.projectUrl = null;
+          this.$refs.form.resetValidation();
         }
       } catch (err) {
         console.log(err);
