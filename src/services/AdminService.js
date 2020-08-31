5a772e4a3b4b49a26c9100d057395bd61be29b54
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-08-31 17:13:03 +0200

    removed stuff

--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -11,10 +11,6 @@ export default {
     return Api().get('allprojects', { headers: authHeader() }) 
   },
 
-  downloadProjectBop(project){
-    return Api().post('downloadprojectbop', project, { headers: authHeader() })
-  },
-
   deleteProject(projectId){
     return Api().delete(`deleteproject/${projectId}`, { headers: authHeader() })
   },
@@ -22,25 +18,4 @@ export default {
   deleteUser(userId){
     return Api().delete(`deleteuser/${userId}`, { headers: authHeader() })
   }
-
-  // index (search) {
-  //   return Api().get('atoms', {
-  //     params: {
-  //       search: search
-  //     }
-  //   })
-  // },
-
-  // show(atomId){
-  //   return Api().get(`atoms/${atomId}`)
-  // },
-
-  // post (atom) {
-  //   return Api()
-  //     .post('atoms', atom, { headers: authHeader() })
-  // },
-  
-  // delete(atomId){
-  //   return Api().delete(`atoms/${atomId}`, { headers: authHeader() })
-  // }
 }
