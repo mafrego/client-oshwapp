8619596d65aa54e195c711f2c729d6a37196e07f
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-02 17:39:08 +0200

    renamed

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -8,7 +8,7 @@
       <v-icon>search</v-icon>
     </v-btn>
 
-    <v-btn class="grey ml-2" :to="{name: 'howto'}" title="how-to">
+    <v-btn class="grey ml-2" :to="{name: 'help'}" title="help">
       <v-icon>help</v-icon>
     </v-btn>
 
