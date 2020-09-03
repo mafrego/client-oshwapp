598605f7495b8b5377cf3f62e88d916c1e708398
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-04 00:20:36 +0200

    added detail card

--- src/views/Products/ProductTreeViewText.vue
+++ src/views/Products/ProductTreeViewText.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
+  <div class="font">
     <div @click="nodeClicked" :style="{'margin-left': `${depth * 20}px`}" class="node">
       <span class="product-name">{{node.name}}</span>
-       <img class="product-image ml-5" :src="node.imageUrl" />
+      <img
+        class="product-image ml-5"
+        :src="node.imageUrl"
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
+      />
+
+      <v-card v-if="hover" width="40%" dark elevation-24 class="card">
+          <ul>
+            <li>
+              <span class="product-details">{{node.description}}</span>
+            </li>
+            <li v-if="node.unitCost">
+              <span class="product-details">{{node.unitCost}} {{node.currency}}</span>
+            </li>
+            <li>
+              <span class="product-details">{{node.quantity.low}} items</span>
+            </li>
+          </ul>
+      </v-card>
     </div>
     <div v-if="expanded">
       <product-tree-view-text
@@ -23,13 +42,14 @@ export default {
     node: Object,
     depth: {
       type: Number,
-      default: 0
+      default: 0,
     },
-    dummy: Boolean
+    dummy: Boolean,
   },
   data() {
     return {
       expanded: false,
+      hover: false,
     };
   },
   methods: {
@@ -44,25 +64,33 @@ export default {
 </script>
 
 <style scoped>
+.font {
+  font-family: monospace;
+}
 .node {
-  text-align: left; 
+  text-align: left;
   font-size: 18px;
 }
 .type {
   margin-right: 10px;
 }
-.product-name{
-  font-family: monospace;
+.product-name {
   cursor: pointer;
 }
-.product-image{
+.product-image {
   cursor: pointer;
-  width: 4%;
+  width: 5%;
   height: auto;
   /* center img */
   margin-left: auto;
   margin-right: auto;
   /* transition duration when mouseon */
   transition: 1.5s;
+}
+.card {
+  position: fixed;
+  right: 5%;
+  top: 15%;
+  font-size: 84%;
 }
 </style>
