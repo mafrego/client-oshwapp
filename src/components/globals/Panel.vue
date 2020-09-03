7ed2e589569c42503a96d7988d71a8fe075533c4
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-04 00:23:27 +0200

    round corners and no dark

--- src/components/globals/Panel.vue
+++ src/components/globals/Panel.vue
@@ -1,12 +1,12 @@
 <template>
 
       <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar flat dense rounded class="cyan">
           <v-toolbar-title>{{title}}</v-toolbar-title>
           <slot name="action" />
         </v-toolbar>
 
-        <div class="pl-4 pr-4 pt-2 pb-2">
+        <div class="pl-4 pr-4 pt-2 pb-2" >
             <slot>
             </slot>
             <!-- <slot name="view"></slot> -->
