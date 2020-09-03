ff3ca831f6ff57e3bd650d0d19e549fdf89352da
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-03 16:10:32 +0200

    nothing

--- src/views/Help/HelpVideo.vue
+++ src/views/Help/HelpVideo.vue
@@ -2,6 +2,7 @@
 <div>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/8Pa9x9fZBtY" frameborder="0"
      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     
 </div>
 </template>
 
