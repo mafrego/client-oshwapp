76d9be22fd6711644ca995fb5699c3c248d0b7eb
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-04 00:22:37 +0200

    added tittles and route to video

--- src/views/general/Home.vue
+++ src/views/general/Home.vue
@@ -6,17 +6,17 @@
     <br />
     <p>What can you do?</p>
     <p>search
-    <v-btn :to="{name: 'products'}" class="grey">
+    <v-btn :to="{name: 'products'}" class="grey" title="search">
       <v-icon>search</v-icon>
     </v-btn>
        and visualize open source hardware products</p>
     <p>register, upload a BOM, assemble, manage and share your project</p>
     <p>
       watch the video tutorial
-      <v-btn class="grey">
+      <v-btn  class="grey" :to="{ name: 'videotutorial'}" title="watch video">
         <v-icon>video_settings</v-icon>
       </v-btn> or go to the help page
-      <v-btn class="grey" :to="{name: 'help'}">
+      <v-btn class="grey" :to="{name: 'help'}" title="help">
         <v-icon>help</v-icon>
       </v-btn>
     </p>
