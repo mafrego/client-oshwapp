af9f22092f943cb8fdcf8b9ca69ade8b749f58d5
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-04 15:50:36 +0200

    added list

--- src/views/general/Home.vue
+++ src/views/general/Home.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="home">
-    <h1><img src="../../assets/kaki.png" class="logo">
-    KAKI</h1>
-    <h2>open source hardware web application</h2>
+    <!-- <h1>
+      <img src="../../assets/kaki.png" class="logo" />
+      KAKI
+    </h1> -->
+    <h1>
+      OSHW APP
+    </h1>
+    <h2>open source hardware application</h2>
     <br />
-    <p>What can you do?</p>
-    <p>search
-    <v-btn :to="{name: 'products'}" class="grey" title="search">
-      <v-icon>search</v-icon>
-    </v-btn>
-       and visualize open source hardware products</p>
-    <p>register, upload a BOM, assemble, manage and share your project</p>
-    <p>
-      watch the video tutorial
-      <v-btn  class="grey" :to="{ name: 'videotutorial'}" title="watch video">
-        <v-icon>video_settings</v-icon>
-      </v-btn> or go to the help page
-      <v-btn class="grey" :to="{name: 'help'}" title="help">
-        <v-icon>help</v-icon>
-      </v-btn>
-    </p>
+    <ul>
+      What can you do?
+      <br />
+      <br />
+      <li>
+        Search
+        <v-btn :to="{name: 'products'}" class="grey" title="search">
+          <v-icon>search</v-icon>
+        </v-btn>and visualize open source hardware products.
+      </li>
+      <br />
+      <li>Register, upload a BOM, assemble, manage and share your project.</li>
+      <br />
+      <li>
+        Watch the video tutorial
+        <v-btn class="grey" :to="{ name: 'videotutorial'}" title="watch video">
+          <v-icon>video_settings</v-icon>
+        </v-btn> or go to the help page
+        <v-btn class="grey" :to="{name: 'help'}" title="help">
+          <v-icon>help</v-icon>
+        </v-btn>.
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -30,6 +42,12 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+  text-align: center;
+}
+h2 {
+  text-align: center;
+}
 .logo {
   width: 5%;
 }
