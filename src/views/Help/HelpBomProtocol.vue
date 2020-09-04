2cb16cc1d1327c7edc9af0aa28879770e85420e4
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-04 15:50:49 +0200

    added lista

--- src/views/Help/HelpBomProtocol.vue
+++ src/views/Help/HelpBomProtocol.vue
@@ -4,80 +4,182 @@
     <h1>BOM protocol</h1>
     <br />
     <h2>BOM headers</h2>
-    <br />n.
-    <span class="bold">headerName</span>: allowed header format, not/required, header description
     <br />
-    <br />
-    <ol>
+    <ol start="0">
       <li>
-        <span class="bold">itemNumber</span>: increasing integer starting from 1, required, unique number associated to
+        <span class="bold">headerName</span>:
+        <ul>
+          <li>allowed header format</li>
+          <li>regex</li>
+          <li>required/not required</li>
+          <li>header description</li>
+        </ul>
+      </li>
+      <br />
+      <li>
+        <span class="bold">itemNumber</span>:
+        <ul>
+          <li>increasing integer starting from 1</li>
+          <li>^[0-9]*[1-9][0-9]*$</li>
+          <li>required</li>
+          <li>unique number associated to atom type</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">name</span>: alphanumerical string with underscores and hyphens allowed, required, unique atom name
+        <span class="bold">name</span>:
+        <ul>
+          <li>
+            alphanumeric string comprehensive of 62 characters
+            (A-Z+a-z+0-9, case-sensitive, no blank spaces allowed) plus underscores and hyphens
+          </li>
+          <li>^[-0-9a-zA-Z_]+$</li>
+          <li>required</li>
+          <li>unique atom name</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">description</span>: string including any character except for commas and semicolons, required, brief atom description of its peculiarities
+        <span class="bold">description</span>:
+        <ul>
+          <li>string including any character except for commas and semicolons</li>
+          <li>required</li>
+          <li>^[^,;]+$</li>
+          <li>brief atom description of its peculiarities</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">moq</span>: integer equal or greater than 1, required, minimum order quantity
+        <span class="bold">moq</span>:
+        <ul>
+          <li>integer equal or greater than 1</li>
+          <li>^[0-9]*[1-9][0-9]*$</li>
+          <li>required</li>
+          <li>minimum order quantity</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">quantity</span>: integer equal or greater than 1, required, number of identical atoms
+        <span class="bold">quantity</span>:
+        <ul>
+          <li>integer equal or greater than 1</li>
+          <li>^[0-9]*[1-9][0-9]*$</li>
+          <li>required</li>
+          <li>number of identical atoms</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">unitCost</span>: floating point number, required, cost of single atom
+        <span class="bold">unitCost</span>:
+        <ul>
+          <li>floating point number</li>
+          <li>^(?:[1-9]\d*|0)?(?:\.\d+)?$</li>
+          <li>required</li>
+          <li>cost of single atom</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">totalCost</span>: floating point number, required, sum of cost of all identical atoms if moq equals 1 otherwise unitCost times a multiple of respective moq
+        <span class="bold">totalCost</span>:
+        <ul>
+          <li>floating point number</li>
+          <li>^(?:[1-9]\d*|0)?(?:\.\d+)?$</li>
+          <li>required</li>
+          <li>sum of cost of all identical atoms if moq equals 1 otherwise unitCost times a multiple of respective moq</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">currency</span>: 3 capital character length string according to currency designator
-        <a
-          href="https://en.wikipedia.org/wiki/ISO_4217"
-          target="_blank"
-        >ISO 4217</a>, required, currency relative to
-        <span class="bold">unitCost</span> and
-        <span class="bold">unitCost</span>
+        <span class="bold">currency</span>:
+        <ul>
+          <li>
+            3 capital character length string according to currency designator
+            <a
+              href="https://en.wikipedia.org/wiki/ISO_4217"
+              target="_blank"
+            >ISO 4217</a>
+          </li>
+          <li>[A-Z]{3}</li>
+          <li>required</li>
+          <li>
+            required, currency relative to
+            <span class="bold">unitCost</span> and
+            <span class="bold">totalCost</span>
+          </li>
+        </ul>
       </li>
       <li>
-        <span class="bold">GTIN</span>: 8, 12, 13, or 14 digit length number, not required,
-        <a
-          href="https://en.wikipedia.org/wiki/Global_Trade_Item_Number"
-          target="_blank"
-        >Global Trade Item Number</a>
+        <span class="bold">GTIN</span>:
+        <ul>
+          <li>8, 12, 13, or 14 digit length number</li>
+          <li>^(\d{8}|\d{12}|\d{13}|\d{14})$</li>
+          <li>not required</li>
+          <li>
+            <a
+              href="https://en.wikipedia.org/wiki/Global_Trade_Item_Number"
+              target="_blank"
+            >Global Trade Item Number</a>
+          </li>
+        </ul>
       </li>
       <li>
-        <span class="bold">SKU</span>: any possible string, not required,
-        <a
-          href="https://en.wikipedia.org/wiki/Stock_keeping_unit"
-          target="_blank"
-        >Stock Keeping Unit</a>
+        <span class="bold">SKU</span>:
+        <ul>
+          <li>no standard defined: alphanumeric string with underscores, hyphens and blank spaces allowed</li>
+          <li>^[-0-9a-zA-Z_. /]+$</li>
+          <li>not required</li>
+          <li>
+            <a
+              href="https://en.wikipedia.org/wiki/Stock_keeping_unit"
+              target="_blank"
+            >Stock Keeping Unit</a>
+          </li>
+        </ul>
       </li>
       <li>
-        <span class="bold">vendorUrl</span>: URL with HTTP string, required if relative
-        <span class="bold">link</span> entry is blank,
-        <a
-          href="https://en.wikipedia.org/wiki/URL"
-          target="_blank"
-        >Uniform Resource Locator</a> of atom vendor
+        <span class="bold">vendorUrl</span>:
+        <ul>
+          <li>URL with HTTP string</li>
+          <li>https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&amp;//=]*)</li>
+          <li>
+            required if relative
+            <span class="bold">link</span> entry is blank
+          </li>
+          <li>
+            <a href="https://en.wikipedia.org/wiki/URL" target="_blank">Uniform Resource Locator</a> of atom vendor
+          </li>
+        </ul>
       </li>
       <li>
-        <span class="bold">leadTime</span>: alphanumerical upper case character string according to
-        <a
-          href="https://en.wikipedia.org/wiki/ISO_8601#Durations"
-          target="_blank"
-        >ISO 8601</a>, not required, time required from the placement of an order to arrival of product at your doorstep
+        <span class="bold">leadTime</span>:
+        <ul>
+          <li>
+            alphanumerical upper case character string according to
+            <a
+              href="https://en.wikipedia.org/wiki/ISO_8601#Durations"
+              target="_blank"
+            >ISO 8601</a>
+          </li>
+          <li>^P(?!$)(\d+(?:\.\d+)?Y)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?W)?(\d+(?:\.\d+)?D)?(T(?=\d)(\d+(?:\.\d+)?H)?(\d+(?:\.\d+)?M)?(\d+(?:\.\d+)?S)?)?$</li>
+          <li>not required</li>
+          <li>time required from the placement of an order to arrival of product at your doorstep</li>
+        </ul>
       </li>
       <li>
-        <span class="bold">link</span>: URL with HTTP string, required if relative
-        <span class="bold">vendorUrl</span> entry is blank,
-        <a
-          href="https://en.wikipedia.org/wiki/URL"
-          target="_blank"
-        >Uniform Resource Locator</a> of atom if custom made or not for sale
+        <span class="bold">link</span>:
+        <ul>
+          <li>URL with HTTP string</li>
+          <li>https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&amp;//=]*)</li>
+          <li>
+            required if relative
+            <span class="bold">vendorUrl</span> entry is blank
+          </li>
+          <li>
+            <a href="https://en.wikipedia.org/wiki/URL" target="_blank">Uniform Resource Locator</a> of atom if custom made or not for sale
+          </li>
+        </ul>
       </li>
       <li>
-        <span class="bold">notes</span>: string including any character except for commas and semicolons, not required, text ad libitum
+        <span class="bold">notes</span>:
+        <ul>
+          <li>string including any character except for commas and semicolons</li>
+          <li>^[^,;]+$</li>
+          <li>not required</li>
+          <li>text ad libitum</li>
+        </ul>
       </li>
     </ol>
     <br />
