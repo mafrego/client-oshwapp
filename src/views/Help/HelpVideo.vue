18ec0c4b02dd11a5cf1bfe2fd74dd1e68f823088
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-05 10:34:58 +0200

    center video tutorial

--- src/views/Help/HelpVideo.vue
+++ src/views/Help/HelpVideo.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/8Pa9x9fZBtY" frameborder="0"
-     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-     
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qYj6NwROFwg"
+     frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe> 
 </div>
 </template>
 
@@ -13,5 +13,7 @@ name: ""
 </script>
 
 <style scoped>
-
+div {
+    text-align: center;
+}
 </style>
