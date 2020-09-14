d078275917b5db1960ff9084c49f3acb713d1d42
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-14 12:23:44 +0200

    improved

--- src/views/Help/HelpHowToImages.vue
+++ src/views/Help/HelpHowToImages.vue
@@ -1,42 +1,66 @@
 <template>
-<div class="font text-justify">
+  <div class="font text-justify">
     <h1>Prepare atom and assembly images</h1>
-    <br>
+    <br />
     <ol>
-        <li>If you are not interested in displaying images along with your project you can skip this section.</li>
-        <li>The fastest way to save atom images is using CAD macros (e.g. 
-         <a href="https://github.com/mafrego/docs-oshwapp/blob/master/template/cad/picture.FCMacro" target="_blank">macro</a>
-        working on <a href="https://www.freecadweb.org/" target="_blank">FreeCAD</a> 
+      <li>
+        Images corresponding atoms and assemblies are recommended but not required.
+      </li>
+      <li>
+        The fastest way to save atom images is using CAD macros (e.g.
+        <a
+          href="https://github.com/mafrego/docs-oshwapp/blob/master/template/cad/picture.FCMacro"
+          target="_blank"
+        >macro</a>
+        working on
+        <a href="https://www.freecadweb.org/" target="_blank">FreeCAD</a>
         for taking pictures of all atoms
-        of <a href="https://github.com/machineagency/jubilee/tree/master/frame/cads/STEP" target="_blank">Jubilee</a> 3d printer).
-        </li>
-        <li>Image file name has to be an alphanumeric string comprehensive of 62 characters 
-            (A-Z+a-z+0-9, case-sensitive) plus underscores and hyphens. Blank spaces not allowed.</li>
-        <li>IMPORTANT: image file names have to be <span class="bold">exactly the same</span> as the names of the respective
-             atoms/assemblies (e. g. atom name: "aB-c_D_1x2x3", respective image file name: 
-             "aB-c_D_1x2x3.png").</li>
-        <li>Allowed formats: .svg .png .jpeg .jpg.</li>
-        <li>Maximum file size is 70kB (enough for a 256x256 image).</li>
-        <li>For better visualization transparent background is recommended
-            (e.g. <a href="https://github.com/mafrego/docs-oshwapp/tree/master/template/images" target="_blank">pictures</a> of Jubilee atoms and assemblies).</li>
+        of
+        <a
+          href="https://github.com/machineagency/jubilee/tree/master/frame/cads/STEP"
+          target="_blank"
+        >Jubilee</a> 3d printer).
+      </li>
+      <li>
+        Image filename shall be an alphanumeric string comprehensive of 62 characters
+        plus underscores and hyphens (corresponding regex: ^[-0-9a-zA-Z_]+$).
+      </li>
+      <li>Blank spaces are not allowed.</li>
+      <li>
+        Image file shall have
+        <span class="bold">exactly the same name</span> as the one of the respective
+        atom or assembly.
+      </li>
+      <li>Image filenames and atoms/assemblies name are case sensitive.</li>
+      <li>Allowed file extensions: .svg .png .jpeg .jpg.</li>
+      <li>Example: atom name "aB-c_D_1x2x3", respective image file name "aB-c_D_1x2x3.png".</li>
+      <li>Maximum file size is 70kB (e.g. a 256x256 image).</li>
+      <li>
+        For better visualization transparent background is recommended
+        (e.g.
+        <a
+          href="https://github.com/mafrego/docs-oshwapp/tree/master/template/images"
+          target="_blank"
+        >pictures</a> of Jubilee atoms and assemblies).
+      </li>
     </ol>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "HelpHowToImages"    
-}
+  name: "HelpHowToImages",
+};
 </script>
 
 <style scoped>
 .font {
-    font-family: monospace;
+  font-family: monospace;
 }
 :any-link {
   text-decoration: none;
 }
 .bold {
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
