0dd378e6063f634df047e588576d50a9e632dd4b
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-17 15:20:20 +0200

    user profile and his projects

--- src/views/Admin/AdminUser.vue
+++ src/views/Admin/AdminUser.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <v-btn :to="{ name: 'adminboard'}" color="blue">admin board</v-btn>
+    <br>
     <ul>
       <li v-for="(value, name, index) in user" :key="index">{{name}}: {{value}}</li>
+    </ul>
+    <br />
+    <ul>
+      Projects:
+      <li v-for="(project, index) in projects" :key="index">
+        {{project.name}}: {{project.uuid}}
+        <v-btn @click="delProject(project.uuid)" color="red">delete</v-btn>
+      </li>
     </ul>
   </div>
 </template>
@@ -15,13 +24,26 @@ export default {
   data() {
     return {
       user: {},
+      projects: [],
     };
+  },
+  methods: {
+    async delProject(projectID) {
+      const ret = await AdminService.deleteProject(projectID);
+      if (ret.status === 200) {
+        this.projects = (
+          await AdminService.getUser(this.$store.state.route.params.userId)
+        ).data.projects;
+      }
+    },
   },
   async created() {
     try {
-      this.user = (
+      const data = (
         await AdminService.getUser(this.$store.state.route.params.userId)
       ).data;
+      this.user = data.user;
+      this.projects = data.projects;
     } catch (error) {
       console.log(error);
     }
