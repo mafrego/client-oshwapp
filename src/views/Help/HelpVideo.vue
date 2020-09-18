56541f65b37cf53c635b27570fc4ce1d1df38259
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-18 10:58:39 +0200

    added video

--- src/views/Help/HelpVideo.vue
+++ src/views/Help/HelpVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h2>app presentation</h2>
+    <h2>app presentation</h2>
     <br />
     <iframe
       width="560"
@@ -10,10 +10,21 @@
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
     ></iframe>
-    <br>
+    <br />
+    <br />
+    <h2>upload bom.csv file</h2>
+    <br />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/wfgz2ARMSOY"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
     <br />
     <h2>assembling tutorial</h2>
-    <br>
+    <br />
     <iframe
       width="560"
       height="315"
