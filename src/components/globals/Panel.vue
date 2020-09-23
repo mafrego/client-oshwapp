a3ab5e47063fd1d35e79aafabd3f6bf7875eb851
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-23 17:00:49 +0200

    minor

--- src/components/globals/Panel.vue
+++ src/components/globals/Panel.vue
@@ -7,9 +7,7 @@
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2" >
-            <slot>
-            </slot>
-            <!-- <slot name="view"></slot> -->
+            <slot></slot>
         </div>
       </div>
 
