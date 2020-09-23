4f5b961e488847b81cc8a204cc9998a424642359
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-23 17:03:20 +0200

    new layout and fixed problem with updating

--- src/views/Projects/ProjectViewUpdate.vue
+++ src/views/Projects/ProjectViewUpdate.vue
@@ -1,30 +1,97 @@
 <template>
   <div>
-    <v-layout column>
-      <v-flex xs1>
-        <!-- <v-text-field v-model="name" :rules="[rules.isAlphanumeric, rules.uniqueName]" label="name"></v-text-field> -->
-        <v-text-field v-model="description" :rules="[rules.isDescription]" label="description"></v-text-field>
-        <v-text-field v-model="version" :rules="[rules.isSemanticVersion]" label="version"></v-text-field>
-        <v-text-field v-model="license" :rules="[rules.isAlphanumeric]" label="license"></v-text-field>
-        <v-text-field v-model="country" :rules="[rules.isISO31661]" label="country"></v-text-field>
-        <v-text-field v-model="region" :rules="[rules.isISO31662]" label="region"></v-text-field>
-        <v-text-field v-model="link" :rules="[rules.isHTTP]" label="projectlink"></v-text-field>
+    <v-container>
+      <v-layout row wrap justify-space-between>
+        <v-flex sm7>
+          <v-text-field
+            v-model="description"
+            :rules="[rules.required, rules.isDescription]"
+            dense
+            solo-inverted
+            label="description"
+          ></v-text-field>
+        </v-flex>
+        <v-flex sm2>
+          <v-text-field
+            v-model="version"
+            :rules="[rules.required, rules.isSemanticVersion]"
+            dense
+            solo-inverted
+            label="version"
+          ></v-text-field>
+        </v-flex>
+        <v-flex sm2>
+          <v-text-field
+            v-model="license"
+            :rules="[rules.required, rules.isAlphanumeric]"
+            dense
+            solo-inverted
+            label="license"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
 
-        <v-btn class="yellow" @click="update()" title="save">
-          <v-icon>save</v-icon>
-        </v-btn>
-        <div v-if="this.message" class="ml-2 msg">
-          <span class="green--text">{{message}}</span>
-        </div>
-        <div v-if="error" class="red--text msg ml-2">{{error}}</div>
-      </v-flex>
-    </v-layout>
+      <v-layout row wrap justify-space-between>
+        <v-flex sm2>
+          <v-text-field
+            v-model="country"
+            :rules="[rules.required, rules.isISO31661]"
+            dense
+            solo-inverted
+            label="country"
+          ></v-text-field>
+        </v-flex>
+        <v-flex v-if="region" sm2>
+          <v-text-field
+            v-model="region"
+            :rules="[rules.isISO31662]"
+            dense
+            solo-inverted
+            label="region"
+          ></v-text-field>
+        </v-flex>
+        <v-flex v-if="link" sm7>
+          <v-text-field
+            v-model="link"
+            :rules="[rules.isHTTP]"
+            dense
+            solo-inverted
+            label="link"
+          ></v-text-field>
+        </v-flex>
+        <v-flex v-if="!region" sm2>
+          <v-text-field
+            v-model="addRegion"
+            :rules="[rules.isISO31662]"
+            dense
+            solo-inverted
+            label="add region"
+          ></v-text-field>
+        </v-flex>
+        <v-flex v-if="!link" sm7>
+          <v-text-field
+            v-model="addLink"
+            :rules="[rules.isHTTP]"
+            dense
+            solo-inverted
+            label="add link"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <div v-if="this.message" class="msg green--text">{{message}}</div>
+      <div v-if="error" class="red--text msg">{{error}}</div>
+      <v-btn class="yellow" @click="update()" title="save">
+        <v-icon>save</v-icon>
+      </v-btn>
+    </v-container>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapActions, mapMutations } from "vuex";
 import semverRegex from "semver-regex";
+import ProjectService from "@/services/ProjectService";
 import iso31661 from "iso-3166";
 import iso31662 from "iso-3166/2";
 
@@ -34,22 +101,25 @@ export default {
     return {
       message: "",
       error: "",
+      addRegion: null,
+      addLink: null,
       rules: {
-        required: (value) => !!value || "Required.",
+        required: (value) => !!value || "required",
         uniqueName: (value) =>
           !this.getProjectNames.includes(value) || "name already taken!",
         isDescription: (value) => {
-          const pattern = /^[-a-zA-Z0-9 _.]*$/;
-          if(value) return pattern.test(value) || "Only alphanumeric, dots, hyphens, underscore chars";
-          else return true
+          const pattern = /^[^,;]+$/;
+          if (value)
+            return pattern.test(value) || "any char but commas and semicolons";
+          else return true;
         },
         isAlphanumeric: (value) => {
           const pattern = /^[a-zA-Z0-9_]*$/;
-          if(value) return pattern.test(value) || "Only alphanumeric, dots, hyphens, underscore chars";
-          else return true
+          if (value) return pattern.test(value) || "only alphanumeric";
+          else return true;
         },
         isSemanticVersion: (value) => {
-          if (value) return semverRegex().test(value) || "e.g. 0.0.1";
+          if (value) return semverRegex().test(value) || "x.y.z";
           else return true;
         },
         isISO31661: (value) => {
@@ -63,7 +133,7 @@ export default {
             )
               ret = true;
           });
-          if (value) return ret || "ISO 3166-1 e.g. IT, ITA, 380 or Italy";
+          if (value) return ret || "ISO 3166-1";
           else return true;
         },
         isISO31662: (value) => {
@@ -71,103 +141,145 @@ export default {
           iso31662.forEach((element) => {
             if (element.code == value || element.name === value) ret = true;
           });
-          if (value)
-            return (
-              ret ||
-              "ISO 3166-2 e.g. for county Wicklow in Ireland IE-WW or Wicklow"
-            );
-          else return true;
+          if (value) return ret || "ISO 3166-2";
+          else{
+            this.region = null
+              this.addRegion = null
+             return true;
+          }
         },
         isHTTP: (value) => {
           const pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
           if (value) return pattern.test(value) || "Invalid http link";
-          else return true;
+          else{
+            this.link = null
+            this.addLink = null
+             return true;
+          }
         },
-      }
+      },
     };
   },
   computed: {
-    ...mapGetters(["getProject", "getProjectNames"]),
-      // name: {
-      //   get() {
-      //     return this.$store.state.projects.project.name
-      //   },
-      //   set (value) {
-      //     this.$store.commit('updateProjectName', value)
-      //   }
-      // },
-      description: {
-        get() {
-          return this.$store.state.projects.project.description
-        },
-        set (value) {
-          this.$store.commit('updateProjectDescription', value)
-        }
+    ...mapGetters(["getProject", "getProjectNames", "getErrorUpdateProject"]),
+    // name: {
+    //   get() {
+    //     return this.$store.state.projects.project.name
+    //   },
+    //   set (value) {
+    //     this.$store.commit('updateProjectName', value)
+    //   }
+    // },
+    description: {
+      get() {
+        return this.$store.state.projects.project.description;
       },
-      version: {
-        get() {
-          return this.$store.state.projects.project.version
-        },
-        set (value) {
-          this.$store.commit('updateProjectVersion', value)
-        }
+      set(value) {
+        this.$store.commit("updateProjectDescription", value);
       },
-      license: {
-        get() {
-          return this.$store.state.projects.project.license
-        },
-        set (value) {
-          this.$store.commit('updateProjectLicense', value)
-        }
+    },
+    version: {
+      get() {
+        return this.$store.state.projects.project.version;
       },
-      country: {
-        get() {
-          return this.$store.state.projects.project.country
-        },
-        set (value) {
-          this.$store.commit('updateProjectCountry', value)
-        }
+      set(value) {
+        this.$store.commit("updateProjectVersion", value);
       },
-      region: {
-        get() {
-          return this.$store.state.projects.project.region
-        },
-        set (value) {
-          this.$store.commit('updateProjectRegion', value)
-        }
+    },
+    license: {
+      get() {
+        return this.$store.state.projects.project.license;
       },
-      link: {
-        get() {
-          return this.$store.state.projects.project.link
-        },
-        set (value) {
-          this.$store.commit('updateProjectLink', value)
-        }
+      set(value) {
+        this.$store.commit("updateProjectLicense", value);
       },
+    },
+    country: {
+      get() {
+        return this.$store.state.projects.project.country;
+      },
+      set(value) {
+        this.$store.commit("updateProjectCountry", value);
+      },
+    },
+    region: {
+      get() {
+        return this.$store.state.projects.project.region;
+      },
+      set(value) {
+        this.$store.commit("updateProjectRegion", value);
+      },
+    },
+    link: {
+      get() {
+        return this.$store.state.projects.project.link;
+      },
+      set(value) {
+        this.$store.commit("updateProjectLink", value);
+      },
+    },
   },
   methods: {
     ...mapActions(["updateProject"]),
+    ...mapMutations(["setLoading"]),
     async update() {
-      // console.log(this.atomToUpdate.description)
+      this.message = null;
+      this.error = null;
+      if (
+        // this.project.name &&
+        this.description &&
+        this.version &&
+        this.license &&
+        this.country
+      ) {
+        this.error = null;
+      } else {
+        this.error = "Please fill in all the required fields.";
+        return;
+      }
+      const project = {
+        description: this.description,
+        version: this.version,
+        license: this.license,
+        country: this.country
+      };
+      if (this.region) {
+        project.region = this.region
+      } else if(this.addRegion){
+        project.region = this.addRegion
+      } else {
+        project.region = null
+      }
+      if (this.link) {
+        project.link = this.link
+      } else if(this.addLink){
+        project.link = this.addLink
+      } else {
+        project.link = null
+      }
+      // console.log('this.region:', this.region)
+      // console.log('this.addRegion:', this.addRegion)
+      // console.log(project.region)
+      // console.log('this.link:', this.link)
+      // console.log('this.addLink:', this.addLink)
+      // console.log(project.link)
       try {
-        this.message = "";
-        this.error = "";
-        const project = {
-          description: this.description,
-          version: this.version,
-          license: this.license,
-          country: this.country,
-          region: this.region,
-          link: this.link
-        }
-        const response = await this.updateProject(project);
+        this.setLoading(true)
+        const response = await ProjectService.updateProject(
+          project,
+          this.getProject.uuid
+        );
+        // console.log(response.data);
         if (response.status === 200) {
-          this.message = "project updated";
-        } else {
-          this.error = "project not updated";
+          this.message = "project metadata updated";
+          this.updateProject(response.data);
+          this.addRegion = null
+          this.addLink = null
         }
       } catch (error) {
-        console.log(error);
+        this.error = error.response.data.message;
+      } finally{
+        this.setLoading(false)
       }
     },
   },
