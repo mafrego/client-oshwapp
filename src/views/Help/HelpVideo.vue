3f63a789c346589cdcf908205ca912c26dcc35a7
Marco Fregonese <marco.fregonese@student.uibk.ac.at>
2020-09-30 09:36:30 +0200

    added one video and changed the assemble one

--- src/views/Help/HelpVideo.vue
+++ src/views/Help/HelpVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>app presentation</h2>
+    <h2>Presentation</h2>
     <br />
     <iframe
       width="560"
@@ -12,7 +12,19 @@
     ></iframe>
     <br />
     <br />
-    <h2>upload bom.csv file</h2>
+    <h2>Create project and generate BOM</h2>
+    <br />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/Vch4CQykHDI"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <br />
+    <br />
+    <h2>Upload external BOM</h2>
     <br />
     <iframe
       width="560"
@@ -23,14 +35,15 @@
       allowfullscreen
     ></iframe>
     <br />
-    <h2>assembling tutorial</h2>
+    <br />
+    <h2>Assemble</h2>
     <br />
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/qYj6NwROFwg"
+      src="https://www.youtube.com/embed/YvH5CLUpcbI"
       frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen
     ></iframe>
   </div>
